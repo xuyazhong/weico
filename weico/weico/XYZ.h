9ca1ea824b690410fbdfed84c917bf5dfbd6c33d
xuyazhong <xuyazhong@foxmail.com>
2015-03-22 22:15:05 +0800

    move fly to this

--- weico/weico/XYZ.h
+++ weico/weico/XYZ.h
@@ -9,6 +9,8 @@
 #ifndef weico_XYZ_h
 #define weico_XYZ_h
 
-
+#define kAppKey         @"82966982"
+#define kRedirectURI    @"http://oauth.weico.cc"
+#define kAppSecret      @"72d4545a28a46a6f329c4f2b1e949e6a"
 
 #endif
